6aa423a6289bb3beb1241cbad82e04e41c35e1fb
Evelyn Hung <57160523+EvelynHung-79@users.noreply.github.com>
2024-02-27 15:24:22 +0800

    renderPage by API

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import model_usage
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
